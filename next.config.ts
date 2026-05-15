01d3178ca828ee16039716b40a0a2bcb555b3297
smit thakkar <sthakkar837@gmail.com>
2026-05-15 17:45:34 +0530

    solve

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  distDir: ".next-local",
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com" },
